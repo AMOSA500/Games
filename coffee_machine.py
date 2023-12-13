2454df0b122ca7943fcbc8d6d2af86b8245aba4c
AMOSA500 <naf.amosa@gmail.com>
2023-12-13 20:39:15 +0000

    Cooffii shop

---
+++ coffee_machine.py
@@ -0,0 +1,35 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+menu_instruction = """
+    Welcome to Cooffii Shop - Table Menu?
+    1. Espresso
+    2. Latte
+    3. Cappuccino
+    4. Check resources level
+    0. Turn off the machine 
+
+"""
