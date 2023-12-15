e7ba6f8aaa02669b78f0a1f6d1c829ca5d56ea0b
AMOSA500 <naf.amosa@gmail.com>
2023-12-15 20:39:03 +0000

    Adding a functionality that will keep asking for only coin even if the customers enter a non numerical value

--- coffee_machine.py
+++ coffee_machine.py
@@ -29,7 +29,7 @@ Welcome to Cooffii Shop - Table Menu?
 1. Espresso
 2. Latte
 3. Cappuccino
-4. Check resources level
+4. Check resources report
 0. Turn off the machine\n
 """
 
@@ -50,6 +50,18 @@ def is_resources_enough(drink_resources):
     return True
 
 
+def process_coin():
+    coin_type = {'quarter': 0.25, 'dime': 0.1, 'nickle': 0.05, 'pennie': 0.01}
+    total = 0
+    coin = 0
+    for index, (key,value) in enumerate(coin_type.items()):
+        try:
+            total += int(input(f"How many {key} do you have?"))*value
+        except ValueError as e:
+            print(f'Please insert only coins - {e}')
+    return total
+
+
 start_machine = True
 
 while start_machine:
@@ -59,7 +71,16 @@ while start_machine:
         drink = MENU[coffee[choice - 1]]
         # Check is resource is enough
         if is_resources_enough(drink['ingredients']):
-            pass
+            print(process_coin())
 
+    elif choice == 4:
+        print('-' * 20)
+        print(f"Water: {machine_resources['water']}ml")
+        print(f"Milk: {machine_resources['milk']}ml")
+        print(f"Coffee: {machine_resources['coffee']}g")
+        print(f"Money: ${revenue}")
+        print('=' * 20)
+    elif choice == 0:
+        start_machine = False
     else:
         print('Wrong button')
