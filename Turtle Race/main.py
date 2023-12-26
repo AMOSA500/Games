34b7c44e3e54d00a6943fa0794322011f867c980
AMOSA500 <naf.amosa@gmail.com>
2023-12-26 10:28:23 +0000

    Added a winning display board to the game

--- Turtle Race/main.py
+++ Turtle Race/main.py
@@ -1,40 +1,71 @@
 import CreateTurtles as cT
 import random
+import show_winner as sw
 
-# Initialisation declaration
-Tt = cT.create_turtles()
-Tt.make_screen()
 
-# Variable declaration
-color = Tt.color
-is_race_on = False
+def start_game():
+    # Initialisation declaration
+    tt_obj = cT.create_turtles()
+    tt_obj.make_screen()
 
-# User make a Bet
-choice_color = Tt.screen.textinput(title='Turtle Race: ', prompt='Which Turtle will win the race? '
-                                                                 'Enter a color')
+    # Variable declaration
+    color = tt_obj.color
+    is_race_on = False
 
-# Draw the Finished Line
-cT.draw_finished_line()
+    # User make a Bet
+    choice_color = tt_obj.screen.textinput(title='Turtle Race: ', prompt='Which Turtle will win the race? '
+                                                                         'Enter a color - red, orange, yellow, green, blue,'
+                                                                         'purple')
 
-# Create Turtle Racing Object
-turtle_list = Tt.create_turtle_racer()
+    if choice_color.lower() in color:
+        is_race_on = True
 
-if choice_color in color:
-    is_race_on = True
-else:
-    print("Select color not in the race")
+        # Draw the Finished Line
+        cT.draw_finished_line()
 
-while is_race_on:
-    for tt in turtle_list:
-        if tt.xcor() > 230:
-            win_color = tt.pencolor()
-            if win_color == choice_color:
-                print(f'The winner is Turtle {win_color} and Wins the race')
-            else:
-                print(f'You lose ({choice_color}). The Turtle {win_color} won the race')
-            is_race_on = False
+        # Create Turtle Racing Object
+        turtle_list = tt_obj.create_turtle_racer()
 
-        distance = random.randint(0, 10)
-        tt.forward(distance)
+        while is_race_on:
+            for tt in turtle_list:
+                if tt.xcor() > 230:
+                    win_color = tt.pencolor()
+                    if win_color == choice_color:
+                        sw.show_message(f'The winner is Turtle {win_color} and Wins the race')
+                        exit_game = tt_obj.screen.numinput(title='Exit Game', prompt='Enter 1 to restart or 0 to Exit')
+                        if exit_game == 1:
+                            tt_obj.screen.clear()
+                            tt_obj.turtle_list.clear()
+                            start_game()
+                        else:
+                            is_race_on = False
+                            try:
+                                tt_obj.screen.bye()
+                            except SystemError as e:
+                                print('Game Ended')
+                    else:
+                        sw.show_message(f'You lose ({choice_color}). The Turtle {win_color} won the race')
+                        exit_game = tt_obj.screen.numinput(title='Exit Game', prompt='Enter 1 to restart or 0 to Exit',
+                                                           default=int)
+                        if exit_game == 1:  # Restart
+                            tt_obj.screen.clear()
+                            tt_obj.turtle_list.clear()
+                            start_game()
+                        else:
+                            is_race_on = False
+                            try:
+                                tt_obj.screen.bye()
+                            except SystemError as e:
+                                print('Game Ended')
 
-Tt.screen.exitonclick()
+                distance = random.randint(0, 10)
+                tt.forward(distance)
+
+        tt_obj.screen.exitonclick()
+
+    else:
+        print("Select color not in the race")
+
+
+# Start Game
+start_game()
---
+++ Turtle Race/show_winner.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def show_message(message):
+    messagebox.showinfo(title='The winning turtle', message=message)
+
