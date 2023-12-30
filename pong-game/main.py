ea4b5181f4fd589995dd1677e554dd86a59a9680
AMOSA500 <naf.amosa@gmail.com>
2023-12-30 23:02:42 +0000

    Added a paddle

--- pong-game/main.py
+++ pong-game/main.py
@@ -5,6 +5,12 @@ screen.bgcolor('black')
 screen.setup(800, 600)
 screen.title('Pong Game')
 
+# Create a paddle
+paddle = Turtle('square')
+paddle.penup()
+paddle.color('white')
+paddle.shapesize(stretch_len=20, stretch_wid=100)
+paddle.goto(0,0)
 
 
 screen.exitonclick()
