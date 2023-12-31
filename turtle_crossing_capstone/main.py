26435c7f67c1135a458844571bf5652be7806897
AMOSA500 <naf.amosa@gmail.com>
2023-12-31 12:29:43 +0000

    Created the car traffic class, player class and added screen listener for key press.

--- turtle_crossing_capstone/main.py
+++ turtle_crossing_capstone/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
-
+from player import Player
+import time
 
 # Create the turtle screen
 screen = Screen()
@@ -8,17 +9,18 @@ screen.setup(600, 600)
 screen.colormode(255)
 screen.tracer(0)
 
-car = Turtle()
-car.shape('turtle')
-car.color('black')
-car.penup()
-car.setheading(90)
-car.goto(0, -280)
+# Create a player
+player = Player()
+
+# Create screen listener
+screen.listen()
+screen.onkey(player.go_up, 'Up')
 
 
 is_game_on = True
 while is_game_on:
     screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
 
